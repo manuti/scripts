771a0dece77475b7ed83755434c03d82e7d2b81e
raspberryando <pedrojg84@hotmail.com>
2013-03-27 19:20:11 +0100

    Update Python_001_Penalties.py

--- Python_001_Penalties.py
+++ Python_001_Penalties.py
@@ -1,1 +1,30 @@
 #Penalties
+import random
+goles_equipo_1 = 0
+goles_equipo_2 = 0
+bucle = 0
+
+while True:
+ bucle = bucle + 1
+ portero_1 = random.randint(1,10)
+ portero_2 = random.randint(1,10)
+ 
+ delantero_1 = random.randint(1,10)
+ delantero_2 = random.randint(1,10)   
+ 
+ #Lanzamiento de penaltis
+ if delantero_1 >= portero_1:
+  print ('Gol del equipo 1!')
+  goles_equipo_1 = goles_equipo_1 + 1
+
+ if delantero_2 >= portero_2:
+  print ('Gol del equipo 2!')
+  goles_equipo_2 = goles_equipo_2 + 1
+
+ if (goles_equipo_1 == 10) or (goles_equipo_2 == 10):
+  break
+
+print ('Se acabó el partido! Resultado:')   
+print ('Equipo 1: ' + str(goles_equipo_1))
+print ('Equipo 2: ' + str(goles_equipo_2))
+print ('Lanzamientos: ' + str(bucle))
