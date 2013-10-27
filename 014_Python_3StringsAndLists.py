c585c7f4abb1381e8021b8b0b56df7e81f6c992a
raspberryando <pedrojg84@hotmail.com>
2013-10-27 13:31:48 +0100

    Create 014_Python_3StringsAndLists.py

---
+++ 014_Python_3StringsAndLists.py
@@ -0,0 +1,8 @@
+string = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain.'
+num_a = 22
+num_b = 27
+num_c = 97
+num_d = 102
+
+print string[num_a:num_b+1]
+print string[num_c:num_d+1]
