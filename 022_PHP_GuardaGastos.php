f71cb8bb1ad9a52e40177664770a8ddaec339d23
raspberryando <pedrojg84@hotmail.com>
2014-03-07 23:39:33 +0100

    Update 022_PHP_GuardaGastos.php

--- 022_PHP_GuardaGastos.php
+++ 022_PHP_GuardaGastos.php
@@ -1,6 +1,6 @@
 function GuardaGastos($Supermercado,$Ocio,$Libros,$Cañas,$Viajes,$Otros){
  $gbd = AbreBD_PDO();
- $sentencia = $gbd->prepare("INSERT INTO ".tablaEscrituras." (Fecha, Alquiler, Transporte, Supermercado, Ocio, Libros, Cañas, Viajes, Otros) $
+ $sentencia = $gbd->prepare("INSERT INTO ".tablaEscrituras." (Fecha, Alquiler, Transporte, Supermercado, Ocio, Libros, Cañas, Viajes, Otros) VALUES ('".date("Y-m-d H:i:s")."',0,0,:g_su,:g_oc,:g_li,:g_ca,:g_vi,:g_ot)");
 
  $sentencia->bindValue(':g_su', $Supermercado, PDO::PARAM_STR);
  $sentencia->bindValue(':g_oc', $Ocio, PDO::PARAM_STR);
